273d22cfb89af30da7f05899e8be25668b2f5c48
willydlw <willydlw@comcast.net>
2016-05-19 11:34:41 -0600

    alien ship moves left and right with arrow keypress, range is limited to screen width

--- python/alien_invasion/alien_invasion.py
+++ python/alien_invasion/alien_invasion.py
@@ -18,14 +18,14 @@ def run_game():
 	bg_color = (230, 230, 230)
 
 	# Make a ship
-	ship = Ship(screen)
+	ship = Ship(ai_settings, screen)
 
 	# Start the main loop for the game
 	while True:
 
 		# Watch for keyboard and mouse events
-		gf.check_events()
-
+		gf.check_events(ship)
+		ship.update()
 		gf.update_screen(ai_settings, screen, ship)
 	
 run_game()
--- python/alien_invasion/game_functions.py
+++ python/alien_invasion/game_functions.py
@@ -1,11 +1,35 @@
 import sys
 import pygame
 
-def check_events():
+def check_keydown_events(event, ship):
+	"""Respond to keypresses."""
+	if event.key == pygame.K_RIGHT:
+		ship.moving_right = True
+	elif event.key == pygame.K_LEFT:
+		ship.moving_left = True
+
+def check_keyup_events(event, ship):
+	""" Respond to key releases."""
+	if event.key == pygame.K_RIGHT:
+		# right arrow key release, stop moving ship to the right
+		ship.moving_right = False
+	elif event.key == pygame.K_LEFT:
+		# left arrow key release
+		ship.moving_left = False
+
+
+def check_events(ship):
 	"""Respond to keypresses and mouse events."""
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			sys.exit()
+		elif event.type == pygame.KEYDOWN:
+			# each keypress is registered as a keydown event
+			check_keydown_events(event, ship)		
+		elif event.type == pygame.KEYUP:
+			# key release is a KEYUP event
+			check_keyup_events(event, ship)
+			
 
 def update_screen(ai_settings, screen, ship):
 	"""Update the screen during each pass through the loop."""
