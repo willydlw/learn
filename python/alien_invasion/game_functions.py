ab79038f3915e75fe2a8a415f55ebad1d403114b
willydlw <willydlw@comcast.net>
2016-05-19 18:17:32 -0600

    adding update bullets and limiting number of bullets

--- python/alien_invasion/game_functions.py
+++ python/alien_invasion/game_functions.py
@@ -12,8 +12,9 @@ def check_keydown_events(event, ai_settings, screen, ship, bullets):
 	elif event.key == pygame.K_SPACE:
 		# Create a new bullet and add it to the bullets group
 		# when the spacebar is pressed
-		new_bullet = Bullet(ai_settings, screen, ship)
-		bullets.add(new_bullet) 
+		if len(bullets) < ai_settings.bullets_allowed:
+			new_bullet = Bullet(ai_settings, screen, ship)
+			bullets.add(new_bullet) 
 
 def check_keyup_events(event, ship):
 	""" Respond to key releases."""
@@ -52,3 +53,15 @@ def update_screen(ai_settings, screen, ship, bullets):
 
 	# Make the most recently drawn screen visible
 	pygame.display.flip()
+
+def update_bullets(bullets):
+	"""Update position of bullets and get rid of old bullet"""
+	# update bullet positions
+	bullets.update()
+
+	# Get rid of bullets that have disappeared
+	# Make a copy of the bullets list for iteration
+	# 	because items are being removed from the original list
+	for bullet in bullets.copy():
+		if bullet.rect.bottom <= 0:
+			bullets.remove(bullet)
