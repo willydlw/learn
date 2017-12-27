8d6a46810c0b60721956fd0f222396cea76da770
willydlw <willydlw@comcast.net>
2017-12-27 14:27:36 -0700

    handles SIGINT which is ctrl+c to exit while loop and normally terminate main function. Also handles SIGTERM in same manner.

--- serial/c/linux/single_port/state_machine/main.c
+++ serial/c/linux/single_port/state_machine/main.c
@@ -15,9 +15,11 @@
 */
 
 
-
+#include <errno.h>
+#include <signal.h>
 #include <stdio.h>      // Standard input/output definitions
 #include <stdint.h>     // uint8_t
+#include <string.h>     // memset
 #include <unistd.h>     // usleep
 
 #include "communication_state.h"
@@ -30,8 +32,77 @@
 
 
 
+static volatile int exit_request = 0;
+
+
+/* signal handler */
+static void signal_handler_term(int sig)
+{
+     /* psignal used for debugging purposes
+       debugging console output lets us know this function was
+       triggered. Prints the string message and a string for the
+       variable sig
+       */
+    psignal(sig, "signal_handler_term");
+    if(sig == SIGINT || sig == SIGTERM){
+        exit_request = 1;
+    }
+    
+}
+
+
 
 int main(){
+
+    // signal
+    sigset_t mask;
+    sigset_t orig_mask;
+    
+
+
+    /** struct sigaction{
+    void (*sa_handler)(int);
+    void (*sa_sigaction)(int, siginfo_t *, void*);
+    sigset_t    sa_mask;
+    int     sa_flags;
+    void (*sa_restorer)(void);
+    }
+    */
+    struct sigaction saterm;
+
+    struct sigaction saint;             // SIGINT caused by ctrl + c
+
+    // register the signal handler function
+    memset(&saterm, 0, sizeof(saterm));
+    saterm.sa_handler = signal_handler_term;
+
+    /*  The sigaction() system call is used to change the action taken by a
+       process on receipt of a specific signal.
+    */
+    if(sigaction(SIGTERM, &saterm, NULL) < 0){
+        perror("sigaction saterm ");
+        return 1;
+    }
+    
+
+    memset(&saint, 0, sizeof(saint));
+    saint.sa_handler = signal_handler_term;
+    if(sigaction(SIGINT, &saint, NULL) < 0){
+        perror("sigaction saint");
+        return 1;
+    }
+
+
+    sigemptyset(&mask);
+    sigaddset(&mask, SIGTERM);
+    sigaddset(&mask, SIGINT);
+
+    // set as blocking so that pselect can receive event
+    if(sigprocmask(SIG_BLOCK, &mask, &orig_mask) < 0){
+        perror("sigprocmask");
+        return 1;
+    }
+
 
     // serial
 
@@ -46,7 +117,7 @@ int main(){
 
     // file selector
     fd_set readfds;
-    struct timeval timeout;
+    struct timespec timeout;
     int max_fd;               // largest file descriptor value
     int num_fd_pending;
 
@@ -78,27 +149,13 @@ int main(){
     
 
     max_fd = fd + 1;
+  
 
-    
+    while(!exit_request){
 
-    /*
-    int select(int max_fd, fd_set *input, fd_set *output, fd_set *error,
-           struct timeval *timeout);
-
-           max_fd specifies highest numbered file descriptor in the input, 
-                    output, and error sets
-
-            timeout argument specifies maximum time select should wait 
-            before returning. Will return sooner if fd is available
-
-    select returns the number of file descriptors that have a pending condition
-        or -1 if there was an error
-    */
-
-    FD_ZERO(&readfds);
-
-    while(loopCount < 10){
-
+        /*  can get SIGTERM at this poin, but it will be delivered while
+            in pselect, because SIGTERM is blocked.
+        */
 
 
         /** must call FD_ZERO and FD_SET every time through loop.
@@ -114,12 +171,49 @@ int main(){
         // re-initialize the timeout structure or it will eventually become zero
         // as time is deducted from the data members. timeval struct represents
         // an elapsed time
-        timeout.tv_sec = 2;
-        timeout.tv_usec = 500000;
+        timeout.tv_sec = 2;                     // seconds
+        timeout.tv_nsec = 0;                    // nanoseconds
 
 
-        num_fd_pending = select(max_fd, &readfds, NULL, NULL, &timeout);
+
+
+        /*
+        int pselect(int nfds, fd_set *readfds, fd_set *writefds,
+            fd_set *exceptfds, const struct timespec *timeout,
+            const sigset_t *sigmask);
+
+                nfds nfds is the highest-numbered file descriptor in any of the 
+                    three sets, plus 1.
+
+                timeout argument specifies maximum time select should wait 
+                before returning. Will return sooner if fd is available
+
+        select returns the number of file descriptors that have a pending condition
+            or -1 if there was an error
+        */
+        num_fd_pending = pselect(max_fd, &readfds, NULL, NULL, &timeout, &orig_mask);
         fprintf(stderr, "\n\n%s, select returned %d\n", __FUNCTION__, num_fd_pending);
+
+        if(num_fd_pending < 0 && errno != EINTR){   // EINTR means signal was caught
+            perror("pselect");
+            ++select_fail_count;
+            // should program end here? Don't think we can recover
+            /* Errors:  
+                EBADF - invalid file descriptor. Possible fd closed, or in error state
+                EINTR - signal was caught
+                EINVAL - nfds is negative or timeout value is invalid
+                ENOMEM - unable to allocate memory for internal tables.
+            */
+            break;
+        }
+        else if(exit_request){
+            fprintf(stderr, "received exit request\n");
+            break;
+        }
+        else if(num_fd_pending == 0){
+             ++select_zero_count;
+             continue;
+        }
 
         if(FD_ISSET(fd, &readfds)){
 
@@ -143,19 +237,11 @@ int main(){
             
         }
 
-        if(num_fd_pending > 0){
-            
-        }
-        else if(num_fd_pending == 0){
-            ++select_zero_count;
-        }
-        else{
-            ++select_fail_count;
-        }
 
         ++loopCount;
-        if(loopCount % 10 == 0){
+        if(loopCount % 5 == 0){
             fprintf(stderr, "loopCount: %d\n", loopCount);
+            raise(SIGTERM);  // for testing SIGTERM
         }
     }
 
