c8d729f311b4e82e1b151cb82176219ae69dd0ce
willydlw <willydlw@comcast.net>
2019-06-19 12:07:37 -0600

    added timeout to ping command, handles 100% packet loss, added error handling

--- batman_network/scripts/query_network_reachable_client.py
+++ batman_network/scripts/query_network_reachable_client.py
@@ -2,27 +2,29 @@
 
 import sys
 import rospy
-from batman_network.srv import *
+from batman_network.srv import CommNodeReachable
 
-def query_network_reachable_client(ip_addr, timeout = "2", count = "1"):
+def query_network_reachable_client(ip_addr):
     rospy.wait_for_service('network_reachable')
     try:
         query_reachable = rospy.ServiceProxy("network_reachable",CommNodeReachable)
-        resp1 = query_reachable(ip_addr,timeout, count)
-        print "%s, response: %s"%(query_network_reachable_client.__name__,resp1.reachable)
+        resp1 = query_reachable(ip_addr)
         return resp1
     except rospy.ServiceException, e:
-        print "query network reachable service failed: %s"%e
+        rospy.logerr("query network reachable service failed: %s", e)
+
 
 def usage():
     return "usage: %s [ip_addr]"%sys.argv[0]
 
 
 if __name__ == "__main__":
+    rospy.init_node('network_reachable_client', log_level=rospy.INFO)
     if len(sys.argv) == 2:
         ip_addr = sys.argv[1]
     else:
         print usage()
         sys.exit(1)
-    print "Requesting %s"%(ip_addr)
-    print "main, response: %s"%(query_network_reachable_client(ip_addr))
+    rospy.loginfo("%s reachable?", ip_addr)
+    resp1 = query_network_reachable_client(ip_addr)
+    rospy.loginfo("response, packet loss: %f, avg time: %f", resp1.packet_loss, resp1.avg_time)
--- batman_network/scripts/query_network_reachable_server.py
+++ batman_network/scripts/query_network_reachable_server.py
@@ -1,13 +1,21 @@
 #!/usr/bin/env python
 
-from batman_network.srv import *
+from batman_network.srv import CommNodeReachable
 import rospy
 import sys 
 import paramiko
+import re
 
 
-def create_ping_command(ip_addr, count):
-    ping_command = "ping " + ip_addr + " -c " + count
+def create_ping_command(ip_addr, count = "3", timeout = "2"):
+    # -W Time to wait for a response, in seconds. The option affects  
+    # only  timeout  in  absence  of  any responses, otherwise ping waits for two RTTs
+    ping_command = "ping " + ip_addr + " -c " + count +" -W " + timeout 
+    
+    # filter everything except statistics and the line below
+    ping_command += " | grep transmitted -A1"
+
+    print ping_command
     
     return ping_command 
 
@@ -26,18 +34,71 @@ def run_command(ssh,command):
     return data_buffer
 
 
+def extract_avg_time(thelist):
+
+    avg_time_val = float(-1)
+
+    # string 'min/avg/max' only exists when ping is successful
+    try:
+        index = thelist.index('min/avg/max')
+        thelen = len(thelist)
+
+        # find first and second / 
+        if index <= thelen-2:
+            avg_time_str = thelist[index+2]
+            pos1 = avg_time_str.find('/')
+            pos2 = avg_time_str.find('/', pos1+1)
+            
+            if pos1 != -1 and pos2 != -1:
+                try:
+                    avg_time_val = float(avg_time_str[pos1+1:pos2])
+                except Exception as e:
+                    rospy.logerr("failed to convert avg_time_str: %s to float: %s", avg_time_str, e)
+            else:
+                rospy.logwarn("failed to find / in avg_time_str: %s", avg_time_str)   
+    except ValueError as e:
+        # term not found in the list, packet loss must be 100%
+        pass 
+
+    return avg_time_val
+
+
+def extract_packet_loss(thelist):
+    
+    packet_loss_val = float(-1)
+
+    try:
+        index = thelist.index('loss')
+        if index >= 2:
+            packet_loss_str = thelist[index-2]
+
+            # remove % from packet loss string
+            pos1 = packet_loss_str.find("%")
+            if pos1 != -1:
+                packet_loss_str = packet_loss_str[:pos1]
+            try:
+                packet_loss_val = float(packet_loss_str)
+            except Exception as e:
+                rospy.logerr("failed to convert packet_loss_str: %s to float: %s", packet_loss, e)
+    except ValueError as e:
+        rospy.logwarn("failed to find 'loss' in the list")
+
+    return packet_loss_val
+    
+
+
 
 def handle_network_reachable(req):
     router_user_name = "root"
     router_password = "P@ssw0rd"
 
+    packet_loss_val = -1
+    average_time_val = -1
+
     rospy.loginfo("called handle_network_reachable")
     rospy.loginfo("%s, req ip addr: %s", handle_network_reachable.__name__, req.dest_ip_addr)
-    rospy.loginfo("%s, req ping timeout: %s", handle_network_reachable.__name__, req.ping_timeout)
-    rospy.loginfo("%s, req ping count: %s", handle_network_reachable.__name__, req.ping_count)
 
-
-    ping_command = create_ping_command(req.dest_ip_addr, req.ping_count)
+    ping_command = create_ping_command(req.dest_ip_addr)
 
     rospy.loginfo("ping command: %s", ping_command)
 
@@ -49,42 +110,34 @@ def handle_network_reachable(req):
         rospy.loginfo("sys.argv[1] %s", sys.argv[1])
         ssh.connect(sys.argv[1], username=router_user_name, password=router_password)     
     except Exception as e:
-        rospy.loginfo("rats, no connection")
         rospy.logerr("SSH connection error: %s", e)
-        return False
-        #sys.stderr.write("SSH connection error: {0}".format(e))
-        #sys.exit(2)
-
-    print "here"
+        return packet_loss_val, average_time_val
+        
     output = run_command(ssh, ping_command)
 
-    print "after run command, output"
-    print output
-
+    if output:
+        output_list = output.split()                # split list into strings
+        packet_loss_val = extract_packet_loss(output_list)
+        average_time_val = extract_avg_time(output_list)
+       
     ssh.close()
 
+    rospy.loginfo("packet loss %f, avg time %f", packet_loss_val, average_time_val)
     
-    #print "Returning %s"%(handle_query_count.counter)
-    return False
+    return packet_loss_val, average_time_val
 
 
 def network_reachable_server():
-    rospy.init_node('network_reachable_server', log_level=rospy.DEBUG)
+    rospy.init_node('network_reachable_server', log_level=rospy.INFO)
     
     if len(sys.argv) < 2:
         rospy.logfatal("missing argv[1], router ip address")
         exit(2)
 
-    print "argv[1] " + sys.argv[1]
-
     s = rospy.Service('network_reachable', CommNodeReachable, handle_network_reachable)
-    print "ready to handle network reachable"
     rospy.spin()
 
 
 if __name__ == "__main__":
-
-    numargs = len(sys.argv)
-    print "numargs " + str(numargs)
     network_reachable_server()
 
